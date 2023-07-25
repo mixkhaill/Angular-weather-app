b9cec26c04ecd0e4c004b7fb8ae3d96faab0c717
m.naskrecki <m.naskrecki@reply.com>
2023-07-25 16:39:53 +0200

    new commit

--- src/app/forecastService.service.ts
+++ src/app/forecastService.service.ts
@@ -6,14 +6,42 @@ import { HttpClient } from "@angular/common/http";
 })
 export class ForecastService {
   apiKey: string = "189b55c7828d643dec69d5d89b0a09f9";
+  private forecastData: any;
   constructor(private htttp: HttpClient) {}
 
   getWeather(location) {
     return this.htttp.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
         location +
-        "&appid=" +
+        "&cnt=5&appid=" +
         this.apiKey
     );
+  }
+  setForecastData(data: any) {
+    this.forecastData = data;
+    console.log(this.forecastData);
+  }
+
+  getForecastData() {
+    return this.forecastData;
+  }
+
+  convertKelvinToCelsius(kelvin: number): number {
+    return kelvin - 273.15;
+  }
+
+  getWeatherIconUrl(weatherStatus: string): string {
+    switch (weatherStatus) {
+      case "Clear":
+        return "https://www.angulartraining.com/images/weather/sun.png";
+      case "Rain":
+        return "https://www.angulartraining.com/images/weather/rain.png";
+      case "Clouds":
+        return "https://www.angulartraining.com/images/weather/clouds.png";
+      case "Snow":
+        return "https://www.angulartraining.com/images/weather/snow.png";
+      default:
+        return "https://www.angulartraining.com/images/weather/default.png";
+    }
   }
 }
--- src/app/future/future.component.ts
+++ src/app/future/future.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ForecastService } from "../forecastService.service";
 
 @Component({
   selector: "app-future",
@@ -6,7 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./future.component.css"],
 })
 export class FutureComponent implements OnInit {
-  constructor() {}
+  forecastData: any;
+  constructor(private forecastService: ForecastService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.forecastData = this.forecastService.getForecastData();
+    console.log(this.forecastData, "forecast-future");
+  }
+
+  convertKelvinToCelsius(kelvin: number): number {
+    return this.forecastService.convertKelvinToCelsius(kelvin);
+  }
+  getWeatherIconUrl(weatherStatus: string) {
+    return this.forecastService.getWeatherIconUrl(weatherStatus);
+  }
 }
