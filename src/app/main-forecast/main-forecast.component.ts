8aa601d71f783c15f55ca83bdcae675fbef43034
m.naskrecki <m.naskrecki@reply.com>
2023-07-25 16:50:48 +0200

    added locations array

--- src/app/main-forecast/main-forecast.component.ts
+++ src/app/main-forecast/main-forecast.component.ts
@@ -10,6 +10,7 @@ import { ForecastService } from "../forecastService.service";
 export class MainForecastComponent implements OnInit {
   weatherSearchForm: FormGroup;
   weatherData: any;
+  locations: string[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,8 +26,21 @@ export class MainForecastComponent implements OnInit {
   sendData(formValues) {
     this.forecastService.getWeather(formValues.location).subscribe((data) => {
       this.weatherData = data;
+      this.forecastService.setForecastData(data);
+      this.addLocation(formValues.location);
       console.log(this.weatherData);
     });
-    console.log(formValues.location);
+  }
+
+  addLocation(location: string) {
+    this.locations.push(location);
+  }
+
+  convertKelvinToCelsius(kelvin: number): number {
+    return this.forecastService.convertKelvinToCelsius(kelvin);
+  }
+
+  getWeatherIconUrl(weatherStatus: string) {
+    return this.forecastService.getWeatherIconUrl(weatherStatus);
   }
 }
